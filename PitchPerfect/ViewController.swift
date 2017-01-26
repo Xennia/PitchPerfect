e3c2257769be60bc0096ecaaa0856e4b6a6c9312
Xennia <xeniaberrios@xenias-mac-pro.local>
2017-01-25 21:25:37 -0500

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Xenia Berrios on 2017-01-25.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+    }
+
+}
+
