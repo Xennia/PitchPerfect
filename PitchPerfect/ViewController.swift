4050bd4c502d844ccf05bf135bfa17103dec0b9e
Xennia <xeniaberrios@xenias-mac-pro.local>
2017-01-25 21:50:11 -0500

    Added print statement

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -20,8 +20,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func recordAudio(_ sender: Any) {
+    @IBAction func recordAudio(_ sender: Any)
+    {
+        print("record button was pressed")
     }
-
+    
+    
 }
 
